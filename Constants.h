d6ff118802e8f3e811503dbd0be05e5ef7470eda
medaminben <mohamed.ben-hassine@mailfence.com>
2024-09-04 02:05:35 +0200

    define constants

---
+++ Constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+#include <mutex>
+
+constexpr const float  gravity = 9.8;
+constexpr const float  FPS     = 60;
+constexpr const float  PI      = 3.141592653589793238;
+constexpr const float  damp    = 0.999;
+constexpr const size_t width   = 2736;
+constexpr const size_t height  = 2192;
+
+#endif //CONSTANTS_H
